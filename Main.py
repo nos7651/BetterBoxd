7b8471677e5b9080c497473b7a2a1c9d829ab780
don5082 <don5082@rit.edu>
2025-10-22 15:02:23 -0400

    Added Main.py and opened database in Main.py

---
+++ Main.py
@@ -0,0 +1,33 @@
+import psycopg
+from sshtunnel import SSHTunnelForwarder
+
+username = "don5082"
+password = "C3nt3rfi3ldb8sball3r"
+dbName = "p320_41"
+
+
+try:
+    with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                            ssh_username=username,
+                            ssh_password=password,
+                            remote_bind_address=('127.0.0.1', 5432)) as server:
+        server.start()
+        print("SSH tunnel established")
+        params = {
+            'dbname': dbName,
+            'user': username,
+            'password': password,
+            'host': 'localhost',
+            'port': server.local_bind_port
+        }
+
+
+        conn = psycopg.connect(**params)
+        curs = conn.cursor()
+        print("Database connection established")
+
+        #DB work here....
+
+        conn.close()
+except:
+    print("Connection failed")
