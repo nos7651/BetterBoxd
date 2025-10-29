e5d02c8caca96526b7762a13ab84ff99dee3e8c8
ssebastian24 <143245603+ssebastian24@users.noreply.github.com>
2025-10-28 22:24:27 -0400

    implemented logging in

--- Main.py
+++ Main.py
@@ -1,27 +1,63 @@
-from queries import create_user
-#pip uninstall psycopg -y
-#pip install psycopg[binary] --force-reinstall --no-cache-dir
+from queries import create_user, login
 
 def main_menu():
+    current_user = None
+
     while True:
-        print("\n===== Movie Database =====")
-        print("1. Create new user")
-        print("2. Exit")
-
-        choice = input("Enter your choice: ")
-
-        if choice == "1":
-            username = input("Enter your username: ")
-            password = input("Enter your password: ")
-            first_name = input("Enter your first name: ")
-            last_name = input("Enter your last name: ")
-            email = input("Enter your email: ")
-            create_user(username, password, email, first_name, last_name)
-        elif choice == "2":
-            print("Vamoose!")
-            break
+        if current_user:
+            print(f"\nLogged in as: {current_user}")
+            print("1. Search for movies")
+            print("2. Log out")
+            print("3. Exit")
         else:
-            print("Invalid Option gangy")
+            print("1. Search for movies")
+            print("2. Login")
+            print("3. Create account")
+            print("4. Exit")
+
+        choice = input("Enter your choice: ").strip()
+
+
+        if current_user:
+            if choice == "1":
+                print("Nothing yet")
+
+            elif choice == "2":
+                print(f"Logged out {current_user}.")
+                current_user = None
+
+            elif choice == "3":
+                print("Goodbye!")
+                break
+
+            else:
+                print("Invalid option.")
+
+
+        else:
+            if choice == "1":
+                print("nothing yet")
+
+            elif choice == "2":
+                username = input("Username: ")
+                password = input("Password: ")
+                if login(username, password):
+                    current_user = username
+
+            elif choice == "3":
+                username = input("Enter your username: ")
+                password = input("Enter your password: ")
+                first_name = input("Enter your first name: ")
+                last_name = input("Enter your last name: ")
+                email = input("Enter your email: ")
+                create_user(username, password, email, first_name, last_name)
+
+            elif choice == "4":
+                print("Goodbye!")
+                break
+
+            else:
+                print("Invalid option.")
 
 if __name__ == "__main__":
     main_menu()
