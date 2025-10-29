f241c55f054ca5967b6ffed08a62b90185b12a45
ssebastian24 <143245603+ssebastian24@users.noreply.github.com>
2025-10-28 21:29:46 -0400

    fix

--- queries.py
+++ queries.py
@@ -1,6 +1,6 @@
 from db import get_connection
 
-def create_user(username, password, email, first_name, last_name, ):
+def create_user(username, password, email, first_name, last_name ):
     conn, server = get_connection()
     try:
         with conn.cursor() as cur:
